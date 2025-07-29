518d6b113e21677ba499bea231d85bac2ad99e5d
DraxofLegend <shaunyoon16@gmail.com>
2025-07-30 01:14:05 +0800

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import "../css/Home.css"
 import "../css/NavBar.css"
-import ronnie from "../assets/rronnie.png"
+import picture from "../assets/picture.png"
 
 function Home() {
     return (
         <>
         <div className="content">
-            <h1 className="anim">Must<br></br>Go Gym Now</h1>
+            <h1 className="anim">Iron<br></br>Core Gym</h1>
             <p className="anim">A gym is a facility equipped with exercise machines, weights, 
             and training spaces designed to help people improve their physical fitness.</p>
             <a href="#" className="download-button anim">Join Now</a>
         </div>
-        <img src={ronnie} alt="gay" className="feature-img anim"></img>
+        <img src={picture} alt="picture" className="feature-img anim"></img>
         </>
     )
 }
